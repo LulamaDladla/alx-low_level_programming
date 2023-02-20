c55fd6c51ae9c9a5baf2ec42fb98871e73000bca
LulamaDladla <gugulethululama98@gmail.com>
2023-02-20 14:19:44 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  program that prints code lines using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("proper grammar, but the outcome is a piece of art,\n");
+	Return (0);
+}
